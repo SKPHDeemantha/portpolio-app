9b41fb7539c41b853d5a517852b7c1c0a8b30f48
SKPHDeemantha <22cse0348@ms.sab.ac.lk>
2024-12-28 17:24:16 +0530

    installed tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ProductCard from './components/ProductCard'
+import Homepage from './Home/Homepage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <ProductCard name="Laptop" price="$1055.36" ></ProductCard>
-     <ProductCard></ProductCard>
-     <ProductCard></ProductCard>
-     <ProductCard></ProductCard>
+     <Homepage></Homepage>
     </>
   )
 }
---
+++ src/Home/Homepage.jsx
@@ -0,0 +1,13 @@
+export default function Homepage(){
+    return(
+        <div>
+          <div className="">
+            <span>Hello Everyone<br/>I am Heshan <br/> Deemantha</span>
+
+            <div>
+                <button>Software Engineer</button>
+            </div>
+          </div>
+        </div>
+    )
+}
