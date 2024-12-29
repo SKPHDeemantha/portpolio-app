23de77c03932a27c69dbfd43583963b840108aec
SKPHDeemantha <22cse0348@ms.sab.ac.lk>
2024-12-29 12:48:09 +0530

    Added react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import ProductCard from './components/ProductCard'
 import Homepage from './Home/Homepage'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <Homepage></Homepage>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Homepage/>}/>
+    </Routes>
+    </BrowserRouter>
+    
     </>
   )
 }
--- src/Home/Homepage.jsx
+++ src/Home/Homepage.jsx
@@ -1,13 +1,14 @@
-export default function Homepage(){
-    return(
-        <div>
-          <div className="">
-            <span>Hello Everyone<br/>I am Heshan <br/> Deemantha</span>
-
-            <div>
-                <button>Software Engineer</button>
-            </div>
+export default function Homepage() {
+  return (
+      <div className="w-screen h-screen bg-orange-300 flex items-center justify-center">
+          <div className="bg-red-500 p-8 flex flex-col items-center justify-center rounded-lg shadow-md">
+              <p className="text-white text-center text-lg">
+                  Hello Everyone<br />I am Heshan <br />Deemantha
+              </p>
+              <button className="mt-4 px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
+                  Software Engineer
+              </button>
           </div>
-        </div>
-    )
+      </div>
+  );
 }
